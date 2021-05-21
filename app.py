137e0e44f96abed0528424651b75ad2eea4d8792
Pierre Blaclard <blaclard@gmail.com>
2021-05-21 15:13:49 +0200

    update

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import requests
+from datetime import datetime
+import pandas as pd
+import numpy as np
+
+test_date = '2021-05-21 11:13:00'
+
+taxiFareApiUrl = 'http://localhost:8000/predict'
+
+'''
+'''
+
+st.markdown('''
+Hello, welcome to the Taxi Fare prediction!
+''')
+
+'''
+
+'''
+
+pickup_datetime = st.text_input('What date and what time?')
+pickup_longitude = st.number_input('What is your pickup longitude?')
+pickup_latitude = st.number_input('What is your pickup latitude?')
+dropoff_longitude = st.number_input('What is your dropoff longitude?')
+dropoff_latitude = st.number_input('What is your dropoff latitude?')
+passenger_count = st.number_input('What is your passenger count')
+
+url = 'https://taxifare.lewagon.ai/predict'
+
+if taxiFareApiUrl == 'https://taxifare.lewagon.ai/predict':
+
+    st.markdown(
+        'Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...'
+    )
+
+params = {
+    "pickup_datetime": pickup_datetime,
+        "pickup_longitude": float(pickup_longitude),
+            "pickup_latitude": float(pickup_latitude),
+            "dropoff_longitude": float(dropoff_longitude),
+            "dropoff_latitude": float(dropoff_latitude),
+            "passenger_count": int(passenger_count)
+}
+
+'''
+3. Let's call our API using the `requests` package...
+'''
+r = requests.get(url, params=params).json()
+
+st.write(r["prediction"])
